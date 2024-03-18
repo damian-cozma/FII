546564f46f01f61c0e9181ef0166d11f7c914a8f
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-03-18 17:53:58 +0200

    main.cpp

--- 1st Year /OOP/Lab 3/main.cpp
+++ 1st Year /OOP/Lab 3/main.cpp
@@ -1,77 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include "Math.h"
 #include <iostream>
-#include <cstdarg>
-#include <cstring>
+#include "Math.h"
+using namespace std;
 
-int Math::Add(int a, int b)
+int main()
 {
-	return a + b;
-}
-
-int Math::Add(int a, int b, int c)
-{
-	return a + b + c;
-}
-
-double Math::Add(double a, double b)
-{
-	return a + b;
-}
-
-double Math::Add(double a, double b, double c)
-{
-	return a + b + c;
-}
-
-int Math::Mul(int a, int b)
-{
-	return a * b;
-}
-
-int Math::Mul(int a, int b, int c)
-{
-	return a * b * c;
-}
-
-double Math::Mul(double a, double b)
-{
-	return a * b;
-}
-
-double Math::Mul(double a, double b, double c)
-{
-	return a * b * c;
-}
-
-int Math::Add(int count, ...)
-{
-	va_list args;
-	va_start(args, count);
-
-	int s = 0;
-
-	while (count != 0)
-	{
-		s = s + va_arg(args, int);
-		count--;
-	}
-
-	va_end(args);
-	return s;
-}
-
-char* Math::Add(const char* a, const char* b)
-{
-	if (a == nullptr || b == nullptr)
-	{
-		return nullptr;
-	}
-
-	char* s = new char[strlen(a) + strlen(b) + 1];
-	strcpy(s, a);
-	strcat(s, b);
-	return s;
+	Math x;
+	cout << x.Add(3, 4) << endl;
+	cout << x.Add(1, 2, 3) << endl;
+	cout << x.Add(2.5, 2.6) << endl;
+	cout << x.Add(1.1, 2.2, 3.3) << endl;
+	cout << x.Mul(3, 4) << endl;
+	cout << x.Mul(1, 2, 3) << endl;
+	cout << x.Mul(2.5, 2.6) << endl;
+	cout << x.Mul(1.1, 2.2, 3.3) << endl;
+	cout << x.Add(3, 1, 2, 10) << endl;
+	cout << x.Add("Hello ", "World") << endl;
 }
 
 //Damian Cozma - March 2024
