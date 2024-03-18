d09d1bcb122c76ddf8647c8e76baabb50f59c0e6
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-03-18 22:32:36 +0200

    main.cpp

---
+++ 1st Year /OOP/Lab 1/main.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    FILE* fp;
+    int suma = 0;
+    char num[100];
+
+    fp = fopen("in.txt", "r");
+
+    while (fgets(num, sizeof(num), fp))
+    {
+        int nr = 0;
+
+        for (int i = 0; i < strlen(num); i++)
+        {
+            if (num[i] >= '0' && num[i] <= '9')
+            {
+                nr = nr * 10 + (num[i] - '0');
+            }
+        }
+        suma += nr;
+    }
+
+    printf("%d", suma);
+
+    fclose(fp);
+
+    return 0;
+}
+
+//Damian Cozma - March 2024
