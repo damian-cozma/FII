62ed25c12f01d0f8e2477df87f8559089ced7d18
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-03-18 22:26:53 +0200

    main.cpp

---
+++ 1st Year /OOP/Lab 2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "NumberList.h"
+
+using namespace std;
+
+int main()
+{
+	NumberList n;
+	n.Init();
+	n.Add(5);
+	n.Add(3);
+	n.Add(7);
+	n.Sort();
+	n.Print();
+
+	// 5, 3, 7 -> 3, 5, 7
+}
