9fbce26093c91188291a293ccc80167a81f09755
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-03-24 20:55:35 +0200

    CMMDC.sh

--- 1st Year /SO/Lab 4/CMMDC.sh
+++ 1st Year /SO/Lab 4/CMMDC.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-
+ 
 function CMMDC {
-
-    if [[ $2 -eq 0 ]]; then
+ 
+    if [[ $1 -eq $2 ]]; then
         echo "$1"
+    elif [[ $1 -gt $2 ]]; then
+        CMMDC $(($1 - $2)) $2
     else
-        CMMDC $2 $(($1 % $2))
+        CMMDC $1 $(($2 - $1))
     fi
 }
-
+ 
 if [[ $# -eq 2 ]]; then
     CMMDC=$(CMMDC $1 $2)
     echo "CMMDC pentru $1 si $2 este: $CMMDC"
 else
     read -p "Primul numar: " a
     read -p "Al doilea numar: " b
-
+ 
     CMMDC=$(CMMDC $a $b)
     echo "CMMDC pentru $a si $b este: $CMMDC"
 fi
