4673ce4d2e124571cc9d9be19720cf0c10277ddb
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-03-26 17:54:49 +0200

    main.cpp

---
+++ 1st Year /OOP/Lab 5/main.cpp
@@ -0,0 +1,42 @@
+//UNFINISHED 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Number.h"
+using namespace std;
+
+int main()
+{
+    Number n1("10110010", 2);
+    Number n2("734", 8);
+    Number n3("FF", 16);
+
+    printf("n1 has %d digits and it is written in base %d\n", n1.GetDigitsCount(), n1.GetBase());
+    for (int tr = 0; tr < n1.GetDigitsCount(); tr++)
+    {
+        printf("n1[%d]=%c\n", tr, n1[tr]);
+    }
+    n1.Print();
+    n2.Print(); 
+
+    n1 = (n2 + n3 - n1) + n1;
+    n1.SwitchBase(2);
+    n1.Print();
+    
+    if (n1 > n2) printf("n1 is bigger than n2\n"); else printf("n2 is bigger than n1\n");
+    Number n4 = 12345; 
+    n1 = 255; 
+    n4 += n1;
+    n4.Print();
+    /*
+    n4 = "13579"; // n4 mentains its base (10) and will be 13579
+    n4.Print();
+    --n4; // the first digit from n4 will be remove ==> n4 becomes 3579
+    n4.Print();
+    n4--; // the last digit from n4 will be remove ==> n4 becomes 357
+    n4.Print(); */
+
+    return 0;
+}
+
+//Damian Cozma - March 2024
