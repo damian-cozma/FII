0eacaaaaa7ae737bb3d2a67a8af7d15d7cfb4ed7
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-04-03 16:38:54 +0300

    main.cpp

---
+++ 1st Year /OOP/Lab 6/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Weather.h"
+#include "Volvo.h"
+#include "BMW.h"
+#include "Seat.h"
+#include "Fiat.h"
+#include "RangeRover.h"
+#include "Circuit.h"
+using namespace std;
+
+int main()
+{
+	Circuit c;
+	c.SetLength(10000);
+	c.SetWeather(Weather::Rain);
+	c.AddCar(new Volvo());
+	c.AddCar(new BMW());
+	c.AddCar(new Seat());
+	c.AddCar(new Fiat());
+	c.AddCar(new RangeRover());
+	c.Race();
+	c.ShowFinalRanks(); // it will print the time each car needed to finish the circuit sorted from the fastest car to the slowest.
+	c.ShowWhoDidNotFinish(); // it is possible that some cars don't have enough fuel to finish the circuit
+	return 0;
+}
+
+//Damian Cozma - April 2024
