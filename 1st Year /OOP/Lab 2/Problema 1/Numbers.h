d3802f18b966837816fac2f83f30aa3c9f9edc56
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-05-07 04:05:14 +0300

    Create Numbers.h

---
+++ 1st Year /OOP/Lab 2/Problema 1/Numbers.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Numbers
+{
+    int numbers[10];
+    int count;
+public:
+    void Init();          // count will be 0
+    bool Add(int x);      // adds X to the numbers list and increase the data member count.
+    // if count is bigger or equal to 10, the function will return false
+    void Sort();          // will sort the numbers vector
+    void Print();         // will print the current vector
+};
