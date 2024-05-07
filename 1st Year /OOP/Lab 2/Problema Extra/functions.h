a68f28bb927badf36b601f831ca9a270bb724e6e
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-05-07 18:50:04 +0300

    Create functions.h

---
+++ 1st Year /OOP/Lab 2/Problema Extra/functions.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Movie.h"
+#include <cstdio>
+
+int compName(Movie a, Movie b);
+int compYear(Movie a, Movie b);
+int compIMDB(Movie a, Movie b);
+int compLenght(Movie a, Movie b);
+int compPassed(Movie a, Movie b);
