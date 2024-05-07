67886ca40621b18d1a663c0cd64d39a492300532
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-05-07 05:03:05 +0300

    Create functions.h

---
+++ 1st Year /OOP/Lab 2/Problema 2/functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "header.h"
+
+int compName(header, header);
+int compAvg(header, header);
+int compEng(header, header);
+int compMate(header, header);
+int compIst(header, header);
