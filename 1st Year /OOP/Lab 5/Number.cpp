b10d403567e58c811c03b09e63f66ef69e02c2ee
Damian <149274508+DamianCozma@users.noreply.github.com>
2024-06-04 03:44:05 +0300

    Update Number.cpp

--- 1st Year /OOP/Lab 5/Number.cpp
+++ 1st Year /OOP/Lab 5/Number.cpp
@@ -1,118 +1,241 @@
-//UNFINISHED
-
-#define _CRT_SECURE_NO_WARNINGS
 #include "Number.h"
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
 
-//Number constructor
-
-Number::Number(const char* value, int base)
-{
-	this -> value = new char[strlen(value) + 1];
-	strcpy(this -> value, value);
-	this -> base = base;
-	this->decimal = strtol(value, nullptr, base);
+Number::Number(const char *value, int base) {
+    this->base=base;
+    this->value=new char[strlen(value)+1];
+    strcpy(this->value,value);
+    this->decimal=0;
+    for (int tr=0;tr<strlen(value);tr++) {
+        if (value[tr]>='0' && value[tr]<='9') {
+            this->decimal=this->decimal*base+(value[tr]-'0');
+        } else {
+            this->decimal=this->decimal*base+(value[tr]-'A'+10);
+        }
+    }
 }
 
-Number::Number(const int nr)
-{
-	this -> base = 10;
-	this -> decimal = nr;
-	this -> value = new char[10];
-	_itoa(nr, this -> value, 10);
+Number::Number(int value){
+    this->base=10;
+    this->value=new char[10];
+    strcpy(this->value,to_string(value).c_str());
+    this->decimal=value;
 }
 
-Number::~Number()
-{
-	delete [] this -> value;
+Number::~Number() {
+    delete[] value;
 }
 
-//Operators
-
-Number operator+(const Number& A, const Number& B)
-{
-	int val = A.decimal + B.decimal;
-	int base = max(A.base, B.base);
-
-	char* value = new char[10];
-	_itoa(val, value, base);
-	return Number(value, base);
+char Number::operator[](int index) {
+    return value[index];
 }
 
-Number operator-(const Number& A, const Number& B)
-{
-	int val = A.decimal - B.decimal;
-	int base = max(A.base, B.base);
+void Number::SwitchBase(int newBase) {
+    if (newBase==base) {
+        return;
+    }
+    int temp=decimal;
+    int i=0;
+    while (temp>0) {
+        value[i]=temp%newBase<10?temp%newBase+'0':temp%newBase-10+'A';
+        temp/=newBase;
+        i++;
+    }
+    value[i]='\0';
+    base=newBase;
 
-	char* value = new char[10];
-	_itoa(val, value, base);
-	return Number(value, base);
+    int start=0;
+    int end=strlen(value)-1;
+    while (start<end) {
+        char temp=value[start];
+        value[start]=value[end];
+        value[end]=temp;
+        start++;
+        end--;
+    }
 }
 
-Number operator+=(const Number& A, const Number& B)
-{
-	int val = A.decimal + B.decimal;
-	int base = max(A.base, B.base);
-
-	char* value = new char[10];
-	_itoa(val, value, base);
-	return Number(value, base);
+void Number::Print() {
+    cout<<value<<endl;
 }
 
-bool Number::operator>(const Number& A)
-{
-	return this->decimal > A.decimal;
+int Number::GetDigitsCount() {
+    return strlen(value);
 }
 
-void Number::operator=(const Number& A)
-{
-	this->decimal = A.decimal;
-	this->base = A.base;
-	this->value = new char[strlen(A.value) + 1];
-	strcpy(this->value, A.value);
+int Number::GetBase() {
+    return base;
 }
 
-void Number::operator=(const int nr)
-{
-	this->decimal = nr;
-	this->base = 10;
-	this->value = new char[10];
-	_itoa(nr, this->value, 10);
+Number::Number(const Number &n) {
+    this->base=n.base;
+    this->decimal=n.decimal;
+    this->value=new char[strlen(n.value)+1];
+    strcpy(this->value,n.value);
 }
 
-int Number::operator[](int index)
-{
-	return this->value[index];
+Number::Number(Number &&n) {
+    this->base=n.base;
+    this->decimal=n.decimal;
+    this->value=n.value;
+    n.value=nullptr;
 }
 
-//Get
-
-int Number::GetDigitsCount()
-{
-	return strlen(this->value);
+Number& Number::operator=(Number &&n) {
+    if (this==&n) {
+        return *this;
+    }
+    delete[] value;
+    this->base=n.base;
+    this->decimal=n.decimal;
+    this->value=n.value;
+    n.value=nullptr;
+    return *this;
 }
 
-int Number::GetBase()
-{
-	return base;
+Number& Number::operator=(const Number &n) {
+    if (this==&n) {
+        return *this;
+    }
+    delete[] value;
+    this->base=n.base;
+    this->decimal=n.decimal;
+    this->value=new char[strlen(n.value)+1];
+    strcpy(this->value,n.value);
+    return *this;
 }
 
-//Other functions
-
-void Number::Print()
-{
-	cout << this -> value << endl;
+Number Number::operator+(Number &n) {
+    int newBase=base>n.base?base:n.base;
+    int newDecimal=decimal+n.decimal;
+    Number result(to_string(newDecimal).c_str(),10);
+    result.SwitchBase(newBase);
+    return result;
 }
 
-void Number::SwitchBase(int newBase)
-{
-	char* value = new char[10];
-	_itoa(this->decimal, value, newBase);
-	this->base = newBase;
-	this->value = value;
+Number Number::operator-(Number &n) {
+    int newBase=base>n.base?base:n.base;
+    int newDecimal=decimal-n.decimal;
+    Number result(to_string(newDecimal).c_str(),10);
+    result.SwitchBase(newBase);
+    return result;
 }
 
-//Damian Cozma - March 2024
+bool Number::operator>(Number &n) {
+    return decimal>n.decimal;
+}
+
+bool Number::operator<(Number &n) {
+    return decimal<n.decimal;
+}
+
+bool Number::operator>=(Number &n) {
+    return decimal>=n.decimal;
+}
+
+bool Number::operator<=(Number &n) {
+    return decimal<=n.decimal;
+}
+
+bool Number::operator==(Number &n) {
+    return decimal==n.decimal;
+}
+
+bool Number::operator!=(Number &n) {
+    return decimal!=n.decimal;
+}
+
+Number& Number::operator--() {
+    char *temp=new char[strlen(value)];
+    for (int tr=1;tr<strlen(value);tr++) {
+        temp[tr-1]=value[tr];
+    }
+    temp[strlen(value)-1]='\0';
+    delete[] value;
+    value=temp;
+    decimal/=10;
+    return *this;
+}
+
+Number Number::operator--(int) {
+    char *temp=new char[strlen(value)];
+    for (int tr=0;tr<strlen(value)-1;tr++) {
+        temp[tr]=value[tr];
+    }
+    temp[strlen(value)-1]='\0';
+    delete[] value;
+    value=temp;
+    decimal/=10;
+    return *this;
+}
+
+Number& Number::operator+=(Number &n) {
+    int val = decimal + n.decimal;
+    int base = this->base > n.base ? this->base : n.base;
+
+    char *temp = new char[10];
+    int i = 0;
+    while (val > 0) {
+        temp[i] = val % base < 10 ? val % base + '0' : val % base - 10 + 'A';
+        val /= base;
+        i++;
+    }
+    temp[i] = '\0';
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char t = temp[start];
+        temp[start] = temp[end];
+        temp[end] = t;
+        start++;
+        end--;
+    }
+    delete[] value;
+    value = temp;
+    decimal = val;
+    this->base = base;
+    return *this;
+}
+
+Number& Number::operator-=(Number &n) {
+    int val = decimal - n.decimal;
+    int base = this->base > n.base ? this->base : n.base;
+
+    char *temp = new char[10];
+    int i = 0;
+    while (val > 0) {
+        temp[i] = val % base < 10 ? val % base + '0' : val % base - 10 + 'A';
+        val /= base;
+        i++;
+    }
+    temp[i] = '\0';
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char t = temp[start];
+        temp[start] = temp[end];
+        temp[end] = t;
+        start++;
+        end--;
+    }
+    delete[] value;
+    value = temp;
+    decimal = val;
+    this->base = base;
+    return *this;
+}
+
+Number::Number(const char *value) {
+    this->base=10;
+    this->value=new char[strlen(value)+1];
+    strcpy(this->value,value);
+    this->decimal=0;
+    for (int tr=0;tr<strlen(value);tr++) {
+        this->decimal=this->decimal*10+(value[tr]-'0');
+    }
+}
+
+// Damian Cozma - March 2024
